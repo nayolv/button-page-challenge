785507a2c703c097150893c7e469edc5a2b881af
Nayelli Olvera <nattkeres@gmail.com>
2023-07-03 19:57:42 -0600

    update

--- src/routes/Root.tsx
+++ src/routes/Root.tsx
@@ -1,11 +1,10 @@
-import { Link, Outlet } from "react-router-dom";
+import { NavLink, Outlet } from "react-router-dom";
 import "../sass/sidebar/sidebar.scss";
 import { menuData } from "../helpers/menuData";
 
 export const Root = () => {
   return (
     <>
-      {/* <div id="sidebar"> */}
       <div className="sidebar-container">
         <p>
           <span>Dev</span>challenges.io
@@ -15,13 +14,12 @@ export const Root = () => {
           <ul>
             {menuData.map((el, i) => (
               <li key={i}>
-                <Link to={el.path}>{el.title}</Link>
+                <NavLink to={el.path}>{el.title}</NavLink>
               </li>
             ))}
           </ul>
         </nav>
       </div>
-      {/* </div> */}
 
       <div id="detail">
         <Outlet />
